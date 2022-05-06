aceaf307ee48c775e0e18cb10f9cc330bb8c8793
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-05-06 09:50:10 +0200

    pr-fix: update with yaml config file

--- build.fsx
+++ build.fsx
@@ -2,16 +2,20 @@
 nuget Fake.DotNet.Cli
 nuget Fake.IO.FileSystem
 nuget Fake.Core.Target
-nuget Fake.DotNet.Testing.Expecto //"
+nuget Fake.DotNet.Testing.Expecto
+nuget FSharp.Configuration //"
 #load ".fake/build.fsx/intellisense.fsx"
 open System
 open Fake.Core
 open Fake.DotNet
 open Fake.IO
-open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
-open Fake.DotNet.Testing
+open FSharp.Configuration
+
+type Yaml = YamlConfig<"build/build.yml">
+let yaml = Yaml()
+let project = yaml.variables.Project
 
 Target.initEnvironment ()
 
@@ -30,7 +34,7 @@ Target.create "Compile" <| fun _ ->
     |> Seq.iter (DotNet.build id)
 
 Target.create "Tests" <| fun _ ->
-  let result = DotNet.exec id "run" "--project .\src\Library.Tests.Unit\Library.Tests.Unit.fsproj --nunit-summary TestResults.xml"
+  let result = DotNet.exec id "run" $"--project .\src\{project}.Tests.Unit\{project}.Tests.Unit.fsproj --nunit-summary TestResults.xml"
   if not result.OK then failwith (String.Join (", ", result.Messages))
 
 Target.create "All" ignore
